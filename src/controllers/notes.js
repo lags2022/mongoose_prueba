095753452ebdd1d2c134e08b8ce3ad1115bc22eb
lags2022 <lguzman.58erb@outlook.com>
2023-03-04 11:46:55 -0500

    update

--- src/controllers/notes.js
+++ src/controllers/notes.js
@@ -6,4 +6,35 @@ const getNotes = async () => {
   return notes;
 };
 
-module.exports = { getNotes };
+const postNote = async (note) => {
+  const newNote = new Note({
+    content: note.content,
+    date: new Date(),
+    important: note.important || false,
+  });
+  const savedNote = await newNote.save();
+  return savedNote;
+};
+
+const getNoteById = async (id) => {
+  const noteid = await Note.findById(id);
+  return noteid;
+};
+
+const deleNoteById = async (id) => {
+  const noteid = await Note.findByIdAndRemove(id); //findByIdAndDelete
+  return noteid;
+};
+
+const modifyNoteById = async (id, note) => {
+  const newNoteInfo = {
+    content: note.content,
+    important: note.important,
+  };
+  const newNoteMod = await Note.findByIdAndUpdate(id, newNoteInfo, {
+    new: true,
+  });
+  return newNoteMod;
+};
+
+module.exports = { getNotes, postNote, getNoteById, deleNoteById,modifyNoteById };
--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,5 +1,11 @@
 const { Router } = require("express");
-const { getNotes } = require("../controllers/notes");
+const {
+  getNotes,
+  postNote,
+  getNoteById,
+  deleNoteById,
+  modifyNoteById
+} = require("../controllers/notes");
 
 const router = Router();
 
@@ -9,6 +15,52 @@ router.get("/", async (req, res) => {
     res.status(200).json(notes);
   } catch (error) {
     res.status(400).json({ message: "Something went wrong, try again" });
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const note = await getNoteById(req.params.id);
+    res.status(200).json(note);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const note = await postNote(req.body);
+    res.status(201).json(note);
+  } catch (error) {
+    res.status(400).json({ message: "Something went wrong, try again" });
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const note = await deleNoteById(req.params.id);
+    res.status(204).end();
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const note = await modifyNoteById(req.params.id, req.body);
+    res.status(200).json(note);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.use((error, req, res, next) => {
+  console.error(error);
+  if (error.name === "CastError") {
+    res.status(400).json({ error: "Malformatted id" });
+  } else {
+    //el 500 es para saber que el error es nuestro
+    res.status(500).json({ error: "Something went wrong, try again" });
   }
 });
 
